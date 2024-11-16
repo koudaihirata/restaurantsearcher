71ed6fd77fa08ed87b292483263d4a3abffa201e
koudaihirata <2230051@ecc.ac.jp>
2024-11-16 23:27:51 +0900

    add:apiから取ってきた情報をフロントで表示

--- src/app/components/ShopInfo.tsx
+++ src/app/components/ShopInfo.tsx
@@ -1,13 +1,23 @@
 import Image from "next/image"
 
+interface ShopInfoProps {
+    shop: {
+        name: string;
+        id: string;
+        photo: {
+            pc: {
+                l: string;
+            };
+        };
+    };
+}
 
-
-export default function ShopInfo() {
+export default function ShopInfo({shop}: ShopInfoProps) {
     return(
         <section className="w-56 border-solid border-red-300 border-2">
-            <Image src={"https://imgfp.hotp.jp/IMGH/64/58/P044216458/P044216458_238.jpg"} alt="店画像" width={150} height={150}/>
-            <h2>割烹料理 すし割烹藤 栄錦</h2>
-            <p>ID:J003763286</p>
+            <Image src={shop.photo.pc.l} alt="店画像" width={150} height={150}/>
+            <h2>{shop.name}</h2>
+            <p>ID:{shop.id}</p>
         </section>
     )
 }
--- src/app/components/test.tsx
+++ src/app/components/test.tsx
@@ -2,17 +2,27 @@
 
 import axios from "axios";
 import ShopInfo from "./ShopInfo";
+import { useState } from "react";
 
 export default function Test() {
+    const [ shopData, setShopData ] = useState([]);
+
     const getData = async () => {
         const response = await axios.get("/api/search");
         console.log(response.data.results.shop);
-      };
+
+        setShopData(response.data.results.shop);
+    };
+    
 
     return (
         <>
             <button onClick={getData} className="w-16 border-solid border-red-300 border-2">ゲット</button>
-            <ShopInfo />
+            <div className="w-full flex flex-wrap">
+                {shopData.map((shop, index) => (
+                    <ShopInfo key={index} shop={shop}/>
+                ))}
+            </div>
         </>
     );
 }
