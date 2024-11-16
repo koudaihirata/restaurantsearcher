3e6ecf0d2ec8276436036f0737adb37d9f9a2227
koudaihirata <2230051@ecc.ac.jp>
2024-11-16 23:54:45 +0900

    add:位置情報取得&表示完了

--- src/app/components/Position.tsx
+++ src/app/components/Position.tsx
@@ -1,15 +1,29 @@
 'use client';
 
-export default function Position() {
-    const getPos = () => {
+import { useState } from "react";
 
+export default function Position() {
+    const [latitude, setLatitude] = useState<number>(0);
+    const [longitude, setLongitude] = useState<number>(0);
+
+    const getPos = () => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                setLatitude(position.coords.latitude);
+                setLongitude(position.coords.longitude);
+            },
+            (error) => {
+                alert("位置情報が取得できませんでした");
+                console.error(error);
+            }
+        );
     } 
 
     return(
         <div className="m-16"> 
             <button onClick={getPos} className="w-16 mb-8 border-solid border-red-300 border-2">現在位置を取得する</button>
-            <p>緯度:?</p>
-            <p>経度:??</p>
+            <p>緯度:{latitude}</p>
+            <p>経度:{longitude}</p>
         </div>
     )
 }
