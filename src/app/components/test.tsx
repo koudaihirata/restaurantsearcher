68f0d5f97af4c93378c03a25fd0d209831227abd
koudaihirata <2230051@ecc.ac.jp>
2024-11-16 21:42:39 +0900

    update：api作成

--- src/app/components/test.tsx
+++ src/app/components/test.tsx
@@ -1,10 +1,16 @@
 'use client';
 
+import axios from "axios";
+
 export default function Test() {
+    const getData = async () => {
+        const response = await axios.get("/api/search");
+        console.log(response.data);
+      };
 
     return (
         <>
-            <p>aaa</p>
+            <button onClick={getData}>ゲット</button>
         </>
     );
 }
