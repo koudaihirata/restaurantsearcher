ef0b8f7f02633badad758d6c0e84deb78cf64399
koudaihirata <2230051@ecc.ac.jp>
2024-11-16 22:35:06 +0900

    店情報を表示

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,9 +1,15 @@
 import { NextResponse } from "next/server";
+import axios from "axios";
 
 const API_KEY = process.env.HOTPEPPER_API_KEY;
 
 export async function GET() {
-  return NextResponse.json({
-    message: API_KEY,
-  })
+  try {
+    const requestData = await axios.get(`https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=${API_KEY}&keyword=寿司&format=json`);
+    const responseData = requestData.data;
+    return NextResponse.json(responseData);
+  } catch (error) {
+    console.log(error);
+    
+  }
 }
--- src/app/components/test.tsx
+++ src/app/components/test.tsx
@@ -5,7 +5,7 @@ import axios from "axios";
 export default function Test() {
     const getData = async () => {
         const response = await axios.get("/api/search");
-        console.log(response.data);
+        console.log(response.data.results.shop);
       };
 
     return (
