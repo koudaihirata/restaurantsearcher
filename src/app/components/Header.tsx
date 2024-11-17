a1a17b1285b7d0535cf2377ee5123ca1c07f0ca3
koudaihirata <2230051@ecc.ac.jp>
2024-11-17 21:14:21 +0900

    add:fontAwesomeのインストール change:依存関係の問題でReact19からReact18にバージョンダウン

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 
 
 export default function Header() {
     return(
         <header>
-            <p>aaa</p>
+            <Image src="/ロゴ.png" alt="ロゴ" width={40} height={40} />
         </header>
     )
 }
