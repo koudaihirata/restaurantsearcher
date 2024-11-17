5df17b5dd78fa7994630ef6d396c6ff16059c941
koudaihirata <2230051@ecc.ac.jp>
2024-11-18 00:32:40 +0900

    add:フラグを使用してナビを表示非表示にする

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useState } from "react";
 import { mainColor, subColor } from "@/app/style/color";
 import { faBars, faHouse, faMap, faRectangleList } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -5,14 +8,20 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
+    const [nabFlag, setNabFlag] = useState<boolean>(true);
+    
+    const handleNab = () => {
+        setNabFlag(!nabFlag);
+    }
+
     return (
         <header className="relative">
             <section className="w-full h-14 flex justify-between items-center pl-8 pr-8 fixed top-0 left-0 z-50" style={{backgroundColor:mainColor}}>
-                <FontAwesomeIcon icon={faBars} className="w-8 h-8" style={{color:subColor}} />
+                <FontAwesomeIcon icon={faBars} className="w-8 h-8" style={{color:subColor}} onClick={handleNab}/>
                 <h1><Image src="/ロゴ.png" alt="ロゴ" width={40} height={40} /></h1>
                 <Image src="/ベイマックス.jpg" alt="プロフィール画像" width={40} height={40} className="rounded-full" />
             </section>
-            <div className="w-64 h-screen fixed top-0 z-20 flex flex-col" style={{backgroundColor:mainColor}}>
+            <div className={`w-64 h-screen fixed top-0 ${nabFlag ? '-left-64' : 'left-0'} z-20 flex flex-col transition-all duration-500`} style={{backgroundColor:mainColor}}>
                 <div className="flex items-center gap-4 mt-24 ml-8">
                     <FontAwesomeIcon icon={faHouse} className="w-6 h-6" style={{color:subColor}} />
                     <p className="text-xl" style={{color:subColor}}>HOME</p>
