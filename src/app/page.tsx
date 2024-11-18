3b9682f6e144a0054bd780ee5f820430418769f7
koudaihirata <2230051@ecc.ac.jp>
2024-11-18 14:23:36 +0900

    背景にロゴを設置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { NabFlagContext } from "./components/header/Header";
 import Header from "./components/header/Header";
 import HomeP from "./pages/HomeP/page";
+import Image from "next/image";
 
 export default function Home() {
   const [nabFlag, setNabFlag] = useState<boolean>(true);
@@ -12,7 +13,10 @@ export default function Home() {
     <NabFlagContext.Provider value={{ nabFlag, setNabFlag }}>
       <Header />
       <section className={`mt-14 ${nabFlag ? 'ml-0' : 'ml-64'} transition-all duration-500`}>
-        <HomeP />
+        <Image src="/bgLogo.svg" alt="背景" width={600} height={600} className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0" />
+        <div className="relative z-10">
+          <HomeP />
+        </div>
       </section>
     </NabFlagContext.Provider>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      translate: {
+        '-1/2': '-50%',
+      },
     },
   },
   plugins: [],
