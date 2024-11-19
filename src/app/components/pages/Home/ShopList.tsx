a0bdc153d0be7e1cdc728a740127dbad9148d8f4
koudaihirata <2230051@ecc.ac.jp>
2024-11-19 12:31:15 +0900

    change:shoplistの表示の仕方をわかりやすいように変更 add:shopの詳しい情報を載せるコンポーネントを作成

--- src/app/components/pages/Home/ShopList.tsx
+++ src/app/components/pages/Home/ShopList.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { accentColor, white } from "@/app/style/color";
-import { faTrainSubway } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { accentColor } from "@/app/style/color";
 import axios from "axios";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrainSubway } from "@fortawesome/free-solid-svg-icons";
 
 interface ShopInfoProps {
     id: string;
@@ -19,13 +19,13 @@ interface ShopInfoProps {
     };
 }
 
-function ShopMap() {
+export default function ShopList() {
     const [ shopData, setShopData ] = useState<ShopInfoProps[]>([]);
-    const [latitude, setLatitude] = useState<number>(0);
-    const [longitude, setLongitude] = useState<number>(0);
+    const [ latitude, setLatitude ] = useState<number>(0);
+    const [ longitude, setLongitude ] = useState<number>(0);
 
 
-    const getData = async () => {
+    useEffect(() => {
         navigator.geolocation.getCurrentPosition(
             (position) => {
                 setLatitude(position.coords.latitude);
@@ -36,49 +36,41 @@ function ShopMap() {
                 console.error(error);
             }
         );
-
-        const response = await axios.get(`/api/search?latitude=${latitude}&longitude=${longitude}`);
-        console.log(response.data.results.shop);
-        console.log(`${latitude}と${longitude}`);
-        
-
-        setShopData(response.data.results.shop);
-    };
+    }, [])
 
     useEffect(() => {
-        getData();
-    }, [latitude, longitude])
+        axios
+            .get(`/api/search?latitude=${latitude}&longitude=${longitude}`)
+            .then((response) => {
+                setShopData(response.data.results.shop);
+                console.log("取得した店舗データ:", response.data.results.shop);
+            })
+            .catch((error) => {
+                console.error("APIエラー:", error);
+            });
+    }, [latitude, longitude]);
+
 
     return(
         <>
-        {shopData.map((shop, index) => (
-            <div key={shop.id}>
-                <div className="flex gap-4 ml-8 mr-8 mb-6 pt-8">
-                    <Image src={shop.photo.pc.l} alt="店の画像" width={80} height={80} className="rounded-lg" />
-                    <div className="flex flex-col gap-2">
-                        <h2 className="text-base">{shop.name}</h2>
-                        <p className="text-xs">{shop.access}</p>
-                        <div className="flex items-center gap-2">
-                            <FontAwesomeIcon icon={faTrainSubway} className="w-4 h-4" />
-                            <p className="text-xs">{shop.address}</p>
+            {shopData.map((shop, index) => (
+                <div key={shop.id}>
+                    <div className="flex gap-4 ml-8 mr-8 mb-6 pt-8">
+                        <Image src={shop.photo.pc.l} alt="店の画像" width={80} height={80} className="rounded-lg" />
+                        <div className="flex flex-col gap-2">
+                            <h2 className="text-base">{shop.name}</h2>
+                            <p className="text-xs">{shop.access}</p>
+                            <div className="flex items-center gap-2">
+                                <FontAwesomeIcon icon={faTrainSubway} className="w-4 h-4" />
+                                <p className="text-xs">{shop.address}</p>
+                            </div>
                         </div>
                     </div>
+                    {index < shopData.length - 1 && (
+                        <div className="w-4/5 h-0.5 m-auto" style={{backgroundColor:accentColor}}></div>
+                    )}
                 </div>
-                {index < shopData.length -1 && (
-                    <div className="w-4/5 h-0.5 m-auto" style={{backgroundColor:accentColor}}></div>
-                )}
-            </div>
-        ))}
-        </>
-    )
-}
-
-export default function ShopList() {
-    return(
-        <>
-            <div className="w-2/5 h-70vh min-w-[400px] opacity-90 rounded-lg  overflow-auto" style={{backgroundColor:white}}>
-                <ShopMap />
-            </div>
+            ))}
         </>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 import App from "./app";
+import ShopDetail from "./components/pages/Home/ShopDetail";
 import ShopList from "./components/pages/Home/ShopList";
+import { white } from "./style/color";
 
 export default function Home() {
 
   return (
     <App>
-        <div className="pt-8 pl-10">
-            <ShopList />
+        <div className="pt-8 pl-10 pr-10">
+          <div className="flex gap-8">
+            <div className="w-full h-70vh opacity-90 rounded-lg  overflow-auto" style={{backgroundColor:white}}>
+              <ShopList />
+            </div>
+            <div className="w-full h-70vh opacity-90 rounded-lg  overflow-auto" style={{backgroundColor:white}}>
+              <ShopDetail />
+            </div>
+          </div>
         </div>
     </App>
   );
