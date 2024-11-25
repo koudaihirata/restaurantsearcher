d167290dee4c756516ff5bf7048b66edbe0d0ade
koudaihirata <2230051@ecc.ac.jp>
2024-11-25 18:44:18 +0900

    change:filterのスタイル変更

--- src/app/components/pages/Home/FilterComponent.tsx
+++ src/app/components/pages/Home/FilterComponent.tsx
@@ -23,7 +23,6 @@ const counts = [
     { id: "count3", value: 50, label: "50件" },
     { id: "count4", value: 70, label: "70件" },
     { id: "count5", value: 100, label: "100件" },
-
 ]
 
 export default function FilterComponent({ filterFlag, range, count, handleRangeChange, handleCountChange }: FilterComponentProps) {
