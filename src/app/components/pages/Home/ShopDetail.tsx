dcf23cce0267297ff30669527884db9f3ab8124c
koudaihirata <2230051@ecc.ac.jp>
2024-11-29 10:00:44 +0900

    change:店の詳細も別ファイルで管理

--- src/app/components/pages/Home/ShopDetail.tsx
+++ src/app/components/pages/Home/ShopDetail.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { parseOperatingHours, Schedule } from '@/app/components/hooks/OpeningTimes';
 import Btn from "../../Btn";
 import { useEffect, useState } from "react";
+import { DetailLoading, NotFound } from "../../utils/LoadRendering";
 
 interface ShopInfoProps {
     id: string;
@@ -53,40 +54,11 @@ export default function ShopDetail({ shop, loading, shopData }: ShopDetailProps)
     }, []);
 
     if (loading) {
-        return (
-            <div className="p-10">
-                <div className="flex">
-                    <div className="w-40 h-40 bg-gray-300 animate-pulse rounded-lg"></div>
-                    <div className="ml-4 flex flex-col justify-around">
-                        <section>
-                            <div className="w-32 h-6 bg-gray-300 animate-pulse mb-3"></div>
-                            <div className="w-48 h-4 bg-gray-300 animate-pulse"></div>
-                        </section>
-                    </div>
-                </div>
-                <div className="mt-5 w-full h-4 bg-gray-300 animate-pulse"></div>
-                <div className="mt-5">
-                    <h3 className="text-center text-lg pb-4 pt-2">営業時間</h3>
-                    <ul className="w-full flex flex-wrap justify-center gap-4">
-                        {weekDays.map(day => (
-                            <li key={day} className="flex flex-col items-center">
-                                <div className="w-12 h-4 bg-gray-300 animate-pulse mb-2"></div>
-                                <div className="w-24 h-4 bg-gray-300 animate-pulse"></div>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-        );
+        return <>{DetailLoading()}</>;
     }
 
     if (shopData.length === 0) {
-        return (
-            <div className="w-full h-full flex justify-center items-center flex-col gap-3">
-                <Image src={"/error.png"} alt="404 not found" width={200} height={50} />
-                <h3 className="text-2xl">No stores found in your search</h3>
-            </div>
-        );
+        return NotFound();
     }
 
     if (!shop) {
